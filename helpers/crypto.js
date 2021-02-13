7dd91f0b428410fca7bc4cf106a82543215a3d7b
Andres Mejias <aemabit@gmail.com>
2021-02-13 00:04:07 -0500

    helpers folder

---
+++ helpers/crypto.js
@@ -0,0 +1,106 @@
+module.exports = [
+  {
+    name: "Bitcoin",
+    value: "BTC",
+  },
+  {
+    name: "Ethereum",
+    value: "ETH",
+  },
+  {
+    name: "Tether",
+    value: "USDT",
+  },
+  {
+    name: "Cardano",
+    value: "ADA",
+  },
+  {
+    name: "Ripple",
+    value: "XRP",
+  },
+  {
+    name: "Polkadot",
+    value: "DOT",
+  },
+  {
+    name: "Litecoin",
+    value: "LTC",
+  },
+  {
+    name: "Chainlink",
+    value: "LINK",
+  },
+  {
+    name: "Stellar",
+    value: "XLM",
+  },
+  {
+    name: "BitcoinCash",
+    value: "BCH",
+  },
+  {
+    name: "Dogecoin",
+    value: "DOGE",
+  },
+  {
+    name: "Uniswap",
+    value: "UNI",
+  },
+  {
+    name: "Aave",
+    value: "AAVE",
+  },
+  {
+    name: "EOS",
+    value: "EOS",
+  },
+  {
+    name: "Cosmos",
+    value: "ATOM",
+  },
+  {
+    name: "TRON",
+    value: "TRX",
+  },
+  {
+    name: "VeChain",
+    value: "VET",
+  },
+  {
+    name: "Tezos",
+    value: "XTZ",
+  },
+  {
+    name: "Monero",
+    value: "XMR",
+  },
+  {
+    name: "NEM",
+    value: "XEM",
+  },
+  {
+    name: "IOTA",
+    value: "MIOTA",
+  },
+  {
+    name: "Maker",
+    value: "MKR",
+  },
+  {
+    name: "Neo",
+    value: "NEO",
+  },
+  {
+    name: "Algorand",
+    value: "ALGO",
+  },
+  {
+    name: "Filecoin",
+    value: "FIL",
+  },
+  {
+    name: "Dai",
+    value: "DAI",
+  },
+];
